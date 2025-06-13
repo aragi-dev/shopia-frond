77309fe201ab3a624dfead0d74d6603385de864c
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-13 16:07:43 -0600

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,94 +1,134 @@
 <template>
-  <main class="h-svh grid place-items-center">
-    <h1 class="text-4xl text-purple-700 font-bold mx-auto">Login</h1>
+  <main class="h-svh grid place-items-center gap-12">
+    <h1 class="text-4xl text-zinc-300 font-bold mx-auto mt-auto">
+      Iniciar Sesión
+    </h1>
     <div class="grid mb-auto">
-      <form @submit.prevent="login" class="grid gap-4">
-        <input
-          v-model="username"
-          type="text"
-          id="user"
-          placeholder="Usuario"
-          name="username"
-          autocomplete="username"
-          class="p-2 px-4 border-1 border-zinc-700 text-zinc-400 rounded-md bg-zinc-900 focus:outline-none focus:border-zinc-500"
-          required
-        />
-        <input
-          v-model="password"
-          type="password"
-          id="password"
-          placeholder="Contrasenia"
-          name="password"
-          autocomplete="current-password"
-          class="p-2 px-4 border-1 border-zinc-700 text-zinc-400 rounded-md bg-zinc-900 focus:outline-none focus:border-zinc-500"
-          required
+      <form @submit.prevent="login" class="grid gap-4 grid-flow-row">
+        <InputNumber
+          label="Code"
+          name="code"
+          maxlength="6"
+          v-model="form.code"
+          :error="fieldState.code.error"
+          :touched="fieldState.code.touched"
+          @blur="() => markTouched('code')"
         />
         <button
           type="submit"
-          :disabled="!isFormValid"
-          class="text-gray-400 p-2 rounded-md outline outline-zinc-700 bg-blue-900 transition-all active:scale-95 disabled:bg-zinc-900 disabled:opacity-20 focus:scale-110"
+          :disabled="!isFormValid || loading"
+          :class="[
+            'text-zinc-900 p-2 rounded-lg outline outline-zinc-700 bg-zinc-300 transition-all active:scale-95 disabled:bg-zinc-900 disabled:text-zinc-500 disabled:opacity-20 w-full mt-2',
+            loading
+              ? 'cursor-wait animate-pulse'
+              : isFormValid
+                ? ''
+                : 'cursor-not-allowed',
+          ]"
         >
-          Confirmar
+          <span
+            v-if="loading"
+            class="grid grid-flow-col text-center items-center gap-2"
+          >
+            <Icon
+              name="spinner"
+              size="20"
+              css="fill-zinc-500 animate-spin ml-auto"
+            />
+            <p class="mr-auto">Validando...</p>
+          </span>
+          <span v-else>Validar</span>
         </button>
       </form>
-      <span class="text-zinc-400 font-bold mx-auto my-4">-o-</span>
-      <section class="grid grid-flow-col gap-4 place-content-center">
-        <button
-          placeholder="Google"
-          class="border-1 border-zinc-600 text-white p-2 rounded-md transition-transform hover:scale-110 active:scale-95 focus:outline-none focus:scale-110"
-        >
-          <Icon name="google" size="24" css="fill-zinc-500" />
-        </button>
-        <button
-          placeholder="Facebook"
-          class="border-1 border-zinc-600 text-white p-2 rounded-md transition-transform hover:scale-110 active:scale-95 focus:outline-none focus:scale-110"
-        >
-          <Icon name="facebook" size="24" css="fill-zinc-500" />
-        </button>
-        <button
-          placeholder="Github"
-          class="border-1 border-zinc-600 text-white p-2 rounded-md transition-transform hover:scale-110 active:scale-95 focus:outline-none focus:scale-110"
-        >
-          <Icon name="github" size="24" css="fill-zinc-500" />
-        </button>
-      </section>
-      <router-link
-        to="/home"
-        class="py-2 px-3 rounded-lg transition-transform hover:cursor-pointer hover:scale-125"
-      >
-        home
-      </router-link>
+      <Notification
+        :show="notification.show"
+        :type="notification.type"
+        :msg="notification.msg"
+        @close="notification.show = false"
+      />
     </div>
   </main>
 </template>
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import { useRouter } from "vue-router";
 import Icon from "@/components/Icon.vue";
 import useAuthStore from "@/services/auth";
+import Notification from "@/components/Notification.vue";
+import InputNumber from "@/components/InputNumber.vue";
+import { NotificationType } from "@/utils/enums/NotificationType";
 
-const username = ref("");
-const password = ref("");
 const router = useRouter();
 const auth = useAuthStore();
-const isFormValid = computed(
-  () => username.value.trim() && password.value.trim()
-);
+
+const loading = ref(false);
+const fieldState = reactive({
+  code: { error: false, touched: false },
+});
+
+const notification = ref({
+  show: false,
+  type: NotificationType.SUCCESS as NotificationType,
+  msg: "Hola",
+});
+
+const form = reactive({
+  code: "",
+});
+
+function showNotification(type: typeof notification.value.type, msg: string) {
+  notification.value = { show: true, type, msg };
+  setTimeout(() => (notification.value.show = false), 3000);
+}
+
+function markTouched(field: keyof typeof fieldState) {
+  fieldState[field].touched = true;
+}
+
+function validateForm() {
+  const code = form.code.trim();
+  const isValid = /^\d{6}$/.test(code);
+  fieldState.code.error = !isValid;
+
+  return isValid;
+}
+
+
+const isFormValid = computed(() => validateForm());
+
+function resetFieldState() {
+  (Object.keys(fieldState) as Array<keyof typeof fieldState>).forEach((k) => {
+    fieldState[k].error = false;
+    fieldState[k].touched = false;
+  });
+}
+
+function resetForm() {
+  Object.assign(form, {
+    code: "",
+  });
+  resetFieldState();
+}
 
 const login = async () => {
-  try {
-    await auth.register(username.value, password.value);
-    router.push({ name: "Home" });
-  } catch (error) {
-    console.error(error);
-    alert("Error al iniciar sesión");
+  (Object.keys(fieldState) as Array<keyof typeof fieldState>).forEach(
+    (k) => (fieldState[k].touched = true)
+  );
+  if (validateForm()) {
+    loading.value = true;
+    try {
+      await auth.login({ code: form.code });
+      router.push({ name: "Home" });
+      resetForm();
+    } catch (error: unknown) {
+      let mensaje = "Hubo un error al inicar session";
+      if (error instanceof Error) {
+        mensaje = error.message;
+      }
+      showNotification(NotificationType.ERROR, mensaje);
+    } finally {
+      loading.value = false;
+    }
   }
 };
 </script>
-<style scoped>
-input:-webkit-autofill {
-  box-shadow: 0 0 0px 20px rgb(28, 34, 41) inset !important;
-  -webkit-text-fill-color: #7f7f91 !important;
-  border: 1px solid #ffffff !important;
-}
-</style>
